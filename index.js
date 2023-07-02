73c0c64eb85e2fe9ad1b082e317e551b413f1d50
polariis <polariis@163.com>
2023-07-02 20:49:14 +0800

    add command to build components

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const createCommands = require("./lib/core/createCommands");
 // program.version("1.0.0");
 program.version(require("./package.json").version); // 设置后可使用 --version查看版本号
 
-helpOptions(); // 调用
+helpOptions(); // 调用，增加其他选项
 createCommands(); // 创建其他指令
 
 program.parse(process.argv); // 解析

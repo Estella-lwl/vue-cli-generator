867ab33b6d949a8d0325d827b4a93f82310ce6d2
polariis <polariis@163.com>
2025-03-04 16:42:57 +0800

    remove useless code

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const program = require("commander");
 const helpOptions = require("./lib/core/help");
 const createCommands = require("./lib/core/createCommands");
 
-// program.version("1.0.0");
-program.version(require("./package.json").version); // 设置后可使用 --version查看版本号
+program.version(require("./package.json").version); // 使其可通过--version查看版本号
 
-helpOptions(); // 调用，增加其他选项
+helpOptions(); // 增加其他选项
 createCommands(); // 创建其他指令
 
-program.parse(process.argv); // 解析
+program.parse(process.argv);
